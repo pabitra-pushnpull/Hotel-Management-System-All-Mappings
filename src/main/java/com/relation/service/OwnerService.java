f11a248eaa50766b444a9437550f134bd768cbe0
Pabitra Devraj Panad <pabitrapanad1025.learn@gmail.com>
2026-06-12 20:17:50 +0530

    Removed unnecessary imports

--- src/main/java/com/relation/service/OwnerService.java
+++ src/main/java/com/relation/service/OwnerService.java
@@ -1,13 +1,10 @@
 package com.relation.service;
 
-import java.util.HashSet;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.relation.model.CustomerModel;
-import com.relation.model.CustomerModel;
 import com.relation.model.ManagerModel;
 import com.relation.model.OwnerModel;
 import com.relation.model.WaiterModel;
